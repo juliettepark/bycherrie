18bcc5b1165e2ed249d59eef787b7e18424b8fd5
juliettepark <69448181+juliettepark@users.noreply.github.com>
2023-11-28 17:38:16 -0800

    ready_for_first_test_deploy

---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./App.css";
+
+function Header() {
+    return (
+        <header className="navbar">
+            <h1>bycherrie</h1>
+            <ul className="nav_items">
+                <li>home</li>
+                <li>catalog</li>
+                <li>about</li>
+            </ul>
+        </header>
+    );
+}
+
+export default Header;
